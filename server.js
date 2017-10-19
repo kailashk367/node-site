8270d03344f888f4001e5c85c75c6dc3c9505a2c
kailash <kkalaskar@hurekatek.com>
2017-10-19 21:17:39 +0530

    init node repo

---
+++ server.js
@@ -0,0 +1,33 @@
+/* // import http 
+var http = require('http');
+
+//handling request and response.
+function requestHandle(req, res) {
+	res.end('Hello World from NODE JS!!!!');
+}
+
+// create the server
+
+var server = http.createServer(requestHandle);
+
+//start server and listen on port x
+server.listen(8181, function(){
+	console.log('server started at 8181');	
+}); */
+
+
+////////////EXPRESS/////////////////
+
+var express = require('express');
+var app = express();
+var port = 8181;
+
+app.listen(port, function(){
+	console.log("App Started...");
+});
+
+app.get('/', function(req, res){
+	res.send("hello world from node js");
+});
+
+////////////end express///////
